9ab642b0412e9e4ec97cc1d0b26f8cb208daa696
Yuuki Zayasu <zayasu@act9.co.jp>
2016-10-28 16:18:52 +0900

    Updated.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,33 +1,5 @@
 class MainController < ApplicationController
-    def index
-#         require "net/http"
-#         require "uri"
-#         require "json"
-#
-#         page = 0
-#         while true
-#             page += 1
-#             uri = URI.parse("https://api.github.com/users/yukpiz/gists?page=#{page}")
-#             https = Net::HTTP.new(uri.host, uri.port)
-#             https.use_ssl = true
-#
-#             request = Net::HTTP::Get.new(uri.request_uri)
-#
-#             begin
-#                 response = https.request(request)
-#                 raise response.code if response.code != "200"
-#                 parsed = JSON.parse(response.body)
-#                 break if response.body.size == 0
-#
-#                 parsed.each do |gist|
-#                     files = gist["files"]
-#                     files.each do |key, value|
-#                         puts value["filename"]
-#                     end
-#                 end
-#             rescue Exception => e
-#                 puts e
-#             end
-#         end
-    end
+
+  def index
+  end
 end
